c55a787eca5620275a604e226f7d1c236862aff2
Higor Henrique Prata Gonçalves <higorhenrique.gon@icloud.com.br>
2026-04-01 02:11:34 -0300

    Revert "romsel: fix underscore dirs shown at SD/flashcard root"
    This reverts commit c770885e648c3eeb695b1177eea505e2563e0285.

--- romsel_r4theme/arm9/source/fileBrowse.cpp
+++ romsel_r4theme/arm9/source/fileBrowse.cpp
@@ -181,9 +181,8 @@ void getDirectoryContents(std::vector<DirEntry> &dirContents, const std::vector<
 				const size_t len = ms().romfolder[i].length();
 				if (strncasecmp(currentPath, ms().romfolder[i].c_str(), len) == 0) {
 					if (len > 0 && ms().romfolder[i][len-1] == '/') {
-						// Only a subdirectory (not the root itself)
-						if (currentPath[len] != '\0') inRomPath = true;
-					} else if (currentPath[len] == '/') {
+						inRomPath = true;
+					} else if (currentPath[len] == '/' || currentPath[len] == '\0') {
 						inRomPath = true;
 					}
 				}
@@ -192,8 +191,8 @@ void getDirectoryContents(std::vector<DirEntry> &dirContents, const std::vector<
 				const size_t len = ms().romPath[i].length();
 				if (strncasecmp(currentPath, ms().romPath[i].c_str(), len) == 0) {
 					if (len > 0 && ms().romPath[i][len-1] == '/') {
-						if (currentPath[len] != '\0') inRomPath = true;
-					} else if (currentPath[len] == '/') {
+						inRomPath = true;
+					} else if (currentPath[len] == '/' || currentPath[len] == '\0') {
 						inRomPath = true;
 					}
 				}
